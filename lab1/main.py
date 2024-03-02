7c454a33557fa7cf63019fe9094476a6d728086d
Soff4 <sofa00sofa@gmail.com>
2024-03-02 12:19:53 +0200

    upload updated project with unit tests

--- lab1/main.py
+++ lab1/main.py
@@ -1,10 +1,22 @@
+import unittest
+
 def factorial(x):
+    if x == 0:
+        return 1
 
     if x == 1:
         return 1
-    else:
-        return (x * factorial(x-1))
+
+    return (x * factorial(x-1))
 
 
-num = 6
-print("Факторіал", num, "буде", factorial(num))
+class TestFactorial(unittest.TestCase):
+    def test_factorial(self):
+        self.assertEqual(factorial(0), 1)
+        self.assertEqual(factorial(1), 1)
+        self.assertEqual(factorial(6), 720)
+        self.assertEqual(factorial(10), 3628800)
+        self.assertEqual(factorial(3), 6)
+
+if __name__ == '__main__':
+    unittest.main()
